d641c29f80dcc4df3f53f2e4f8836c313cdf5ff5
Nate Boyette <nateboyette@icloud.com>
2019-03-04 16:12:32 -0800

    Add tests for durability and enhancement

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,12 +1,34 @@
-module.exports = {
-  repair
-}
+const enhancer = {
+  success: function(item) {
+    // switch(item.enhancement) {
+    //   case 16
+    // }
+  },
+
+  fail: function(item) {
+
+    if (item.enhancement < 15 && item.durability < 25) {
+      return { ...item };
+    }
+
+    const durability = item.enhancement < 15 ? item.durability - 5 : item.durability -10
+
+    const enhancement = item.enhancement > 16 ? item.enhancement - 1 : item.enhancement
 
 
-function repair(item) {
-  if (item) {
-    item.durability = 100
+    return {...item, durability, enhancement}
+    
+
+    
+
+  },
+
+  repair: function(item) {
+    item.durability = 100;
+    return item;
   }
+};
 
-  return item
-}
+module.exports = {
+  enhancer
+};
--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,27 +1,52 @@
-const {repair} = require('./enhancer')
+const {
+  enhancer: { success, repair, fail }
+} = require("./enhancer");
 
-describe('enhancer.js', () => {
+describe("enhancer.js", () => {
+  describe("Fail()", () => {
+    // The durability decreased by 5 if the item's enhancement is btw 0 -15
 
-  describe('repair()', () => {
-    test('repairs durability of an item to 100', () => {
+    it("Durability decreases by 5 if enhancement is between 0 and 14", () => {
+      // Arrange (setup)
       const item = {
-        name: 'TestItem',
-        type: 'weapon',
-        durability: 80,
-        enhancement: 12,
-        displayName: '[+12] TestItem'
-      }
+        enhancement: 14,
+        durability: 85
+      };
 
-      const expected = {
-        ...item,
-        durability: 100
-      }
+      // Act
 
-      expect(repair(item)).toEqual(expected)
+      const actual = fail(item);
 
-    })
-  })
+      //Assert
+
+      expect(actual.durability).toBe(80);
+    });
+
+    it("Durability decreases by 10 if enhancement is greater than 15", () => {
+      // Arrange (setup)
+      const item = {
+        enhancement: 16,
+        durability: 85
+      };
+
+      // Act
+
+      const actual = fail(item);
+
+      //Assert
+
+      expect(actual.durability).toBe(75);
+    });
+
+    it('should not affect item if enhancement is less than 15 and the durability is below 25', () => {
+      const item = {
+        enhancement: 14,
+        durability: 24,
+      };
+
+      expect(fail(item)).toEqual(item);
+    });
 
 
-
-})
+  });
+});
